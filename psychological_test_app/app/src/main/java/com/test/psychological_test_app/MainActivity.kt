0d133338cccd067068068420f529a751ff17146e
tkdgns8234 <55433939+tkdgns8234@users.noreply.github.com>
2022-07-03 22:07:40 +0900

    심리테스트 V1.0.1

--- psychological_test_app/app/src/main/java/com/test/psychological_test_app/MainActivity.kt
+++ psychological_test_app/app/src/main/java/com/test/psychological_test_app/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import com.test.psychological_test_app.databinding.ActivityMainBinding
 
+/*
+Jetpack과 함께 소개된 안드로이드 내비게이션은
+Fragment와 Activity 간 구현및 이동을 안정적으로 할 수 있도록 도와주는 컴포넌트이다.
+ */
+
 class MainActivity : AppCompatActivity() {
     // late init 을 사용하는 경우 요소 선언 후 null 값을 대입하지 않아도 된다.
     // null 값을 대입하는 경우 ? 를 추가하여 선언해야하고 noNull 타입에 대입이 안되기 때문에 귀찮은 작업들이 뒤따른다.
