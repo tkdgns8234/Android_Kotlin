8dfa95929843526ceb055ce0b31de3beb1d0dfe7
tkdgns8234 <55433939+tkdgns8234@users.noreply.github.com>
2022-07-11 18:33:55 +0900

    ListView 최적화

--- basic_skill/listview/app/src/main/java/com/hoon/listview/CustomListAdapter.kt
+++ basic_skill/listview/app/src/main/java/com/hoon/listview/CustomListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import com.hoon.listview.databinding.ItemListViewBinding
 
 class CustomListAdapter(val customList : MutableList<CustomModel>) : BaseAdapter() {
@@ -20,11 +21,18 @@ class CustomListAdapter(val customList : MutableList<CustomModel>) : BaseAdapter
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val binding = ItemListViewBinding.inflate(LayoutInflater.from(parent?.context), parent, false)
+        var convertView = convertView
 
-        binding.tv1.text = customList[position].textView1
-        binding.tv2.text = customList[position].textView2
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent?.context).inflate(R.layout.item_list_view, parent, false)
+        }
 
-        return binding.root
+        val tv1 = convertView?.findViewById<TextView>(R.id.tv_1)
+        val tv2 = convertView?.findViewById<TextView>(R.id.tv_2)
+
+        tv1?.text = customList[position].textView1
+        tv2?.text = customList[position].textView2
+
+        return convertView!!
     }
 }
